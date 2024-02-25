059c47fe8073d774ad296210f4ebf20f56a5d16c
Tony <astravius@outlook.com>
2024-02-24 21:00:59 -0800

    added error catching and rectangles

--- src/main/java/com/cmpt276/as2/controllers/UsersControllers.java
+++ src/main/java/com/cmpt276/as2/controllers/UsersControllers.java
@@ -5,14 +5,13 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cmpt276.as2.models.User;
@@ -26,6 +25,13 @@ public class UsersControllers {
     @Autowired
     private UserRepository userRepo;
 
+    // redirect to home
+    @RequestMapping("/")
+    public String home(){
+        return "redirect:/users/view";
+    }
+
+    // view all page
     @GetMapping("/users/view")
     public String getAllUsers(Model model){
         System.out.println("Getting all users");
@@ -36,20 +42,26 @@ public class UsersControllers {
         return "users/showAll";
     }
 
+    // add student
     @PostMapping("/users/add")
     public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response){
         System.out.println("ADD user");
-        String newName = newuser.get("name");
-        int newWeight = Integer.parseInt(newuser.get("weight"));
-        int newHeight = Integer.parseInt(newuser.get("height"));
-        String newHair = newuser.get("hair");
-        double newGpa = Double.parseDouble(newuser.get("gpa"));
-        String newAlive = newuser.get("alive");
-        userRepo.save(new User(newName, newWeight, newHeight, newHair, newGpa, newAlive));
-        response.setStatus(201);
+        try{
+            String newName = newuser.get("name");
+            int newWeight = Integer.parseInt(newuser.get("weight"));
+            int newHeight = Integer.parseInt(newuser.get("height"));
+            String newHair = newuser.get("hair");
+            double newGpa = Double.parseDouble(newuser.get("gpa"));
+            String newAlive = newuser.get("alive");
+            userRepo.save(new User(newName, newWeight, newHeight, newHair, newGpa, newAlive));
+            response.setStatus(201);
+        }catch(Exception e){
+            System.out.println("FORM NOT FILLED");
+        }
         return "redirect:/users/view";
     }
 
+    // edit form prefill
     @GetMapping("/users/edit")
     public String showEdit(@RequestParam("id") int id, Model model){
         System.out.println("Getting user " + id);
@@ -57,19 +69,33 @@ public class UsersControllers {
         model.addAttribute("user", user);
         return "users/editForm";
     }
+    // edit form
     @PostMapping("/users/edit")
-    public String editUser(@ModelAttribute("user") User user){
-        System.out.println("Editing user");
-        userRepo.save(user);
+    public String editUser(@ModelAttribute("user") User user, BindingResult result){
+        if(!result.hasErrors()){
+            System.out.println("Editing user");
+            userRepo.save(user);
+        }else{
+            System.out.println("ERROR EDITING STUDENT");
+        }
         return "redirect:/users/view";
     }
 
+    // remove student
     @PostMapping("/users/remove")
     public String removeUser(@RequestParam("id") int id){
         System.out.println("REMOVE user " + id);
-        // Integer sid = Integer.valueOf(id);
         userRepo.deleteById(id);
-        // return "users/addedUser";
         return "redirect:/users/view";
     }
+
+    // rectangle
+    @GetMapping("/users/rectangle")
+    public String getMethodName(Model model) {
+        System.out.println("Getting all users");
+        List<User> users = userRepo.findAll();
+        model.addAttribute("us", users);
+        return "users/rectangle";
+    }
+    
 }
